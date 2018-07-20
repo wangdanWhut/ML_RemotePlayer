c1777d3407f89c289655c0986900552822b03a6e
王丹 <wangd@500wan.com>
2018-07-20 13:39:29 +0800

    Initial Commit

---
+++ RemotePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RemotePlayer
+//
+//  Created by 王丹 on 2018/7/20.
+//  Copyright © 2018年 王丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
